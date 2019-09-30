10edee4d466e5ba307a9b745be12bc27bdf67cec
Eduardo <esempere@sistemastg.com>
2019-09-30 07:50:55 -0500

    .

--- ABCMicroservicioREST2_WAR/src/main/java/com/stg/abc/AbcMicroservicioRestApplication.java
+++ ABCMicroservicioREST2_WAR/src/main/java/com/stg/abc/AbcMicroservicioRestApplication.java
@@ -2,12 +2,21 @@ package com.stg.abc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
 
 @SpringBootApplication
-public class AbcMicroservicioRestApplication {
+public class AbcMicroservicioRestApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AbcMicroservicioRestApplication.class, args);
+	}
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		 
+		return builder.sources(AbcMicroservicioRestApplication.class);
 	}
 
 }
